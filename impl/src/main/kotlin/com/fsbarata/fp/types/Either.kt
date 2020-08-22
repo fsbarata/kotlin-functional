5b8802fb1f23fd4d8b308efb224fdfe4f09d5bb2
fixedlight <fixedlight@gmail.com>
2020-08-22 11:56:09 +0100

    Either

---
+++ impl/src/main/kotlin/com/fsbarata/fp/types/Either.kt
@@ -0,0 +1,33 @@
+package com.fsbarata.fp.types
+
+import com.fsbarata.fp.concepts.Context
+import com.fsbarata.fp.concepts.Functor
+import com.fsbarata.fp.concepts.Monad
+
+sealed class Either<out T, out A> : Monad<Either<*, *>, A> {
+	data class Left<out T>(val value: T) : Either<T, Nothing>()
+	data class Right<out U>(val value: U) : Either<Nothing, U>()
+
+	override fun <B> just(b: B): Either<T, B> = Right(b)
+
+	override fun <B> map(f: (A) -> B): Either<T, B> =
+			when (this) {
+				is Left -> Left(value)
+				is Right -> Right(f(value))
+			}
+
+	override fun <B> flatMap(f: (A) -> Functor<Either<*, *>, B>): Either<T, B> =
+			when (this) {
+				is Left -> Left(value)
+				is Right -> f(value).asEither
+			}
+
+	companion object {
+		fun <L, R> left(value: L): Either<L, R> = Left(value)
+		fun <L, R> right(value: R): Either<L, R> = Right(value)
+	}
+}
+
+val <A> Context<Either<*, *>, A>.asEither: Either<Nothing, A>
+	get() = this as Either<Nothing, A>
+
