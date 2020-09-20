363c1dc653bf33669ae4ac8f03df856957f30c6e
fixedlight <fixedlight@gmail.com>
2020-09-20 15:06:17 +0100

    improved non empty list implementation

--- impl/src/main/kotlin/com/fsbarata/fp/types/NonEmptyList.kt
+++ impl/src/main/kotlin/com/fsbarata/fp/types/NonEmptyList.kt
@@ -4,38 +4,64 @@ import com.fsbarata.fp.concepts.Context
 import com.fsbarata.fp.concepts.Foldable
 import com.fsbarata.fp.concepts.Functor
 import com.fsbarata.fp.concepts.Monad
+import java.io.Serializable
 
-class NonEmptyList<A> private constructor(
-		private val list: List<A>
-) : Monad<NonEmptyList<*>, A>,
+data class NonEmptyList<A>(
+		private val head: A,
+		private val tail: List<A>
+) : AbstractList<A>(),
+		Monad<NonEmptyList<*>, A>,
 		Foldable<A>,
-		List<A> by list {
+		Serializable,
+		List<A> {
+	override val size: Int = 1 + tail.size
+
+	override fun get(index: Int): A =
+			if (index == 0) head
+			else tail[index - 1]
+
+	override fun isEmpty() = false
+
+	fun first() = head
+	fun firstOrNull(): Nothing = throw UnsupportedOperationException()
+	fun last() = if (tail.isEmpty()) head else tail.last()
+	fun lastOrNull(): Nothing = throw UnsupportedOperationException()
+
 	override fun <B> just(b: B): NonEmptyList<B> =
 			Companion.just(b)
 
 	override fun <B> map(f: (A) -> B): NonEmptyList<B> =
-			NonEmptyList(list.map(f))
+			NonEmptyList(f(head), tail.map(f))
 
 	override fun <B> bind(f: (A) -> Functor<NonEmptyList<*>, B>): NonEmptyList<B> =
-			NonEmptyList(list.flatMap { f(it).asNel })
+			flatMap { f(it).asNel }
 
-	fun <B> flatMap(f: (A) -> NonEmptyList<B>): NonEmptyList<B> =
-			NonEmptyList(list.flatMap(f))
-
-	override fun <R> fold(initialValue: R, accumulator: (R, A) -> R): R =
-			list.fold(initialValue, accumulator)
-
-	override fun equals(other: Any?): Boolean {
-		if (this === other) return true
-		if (javaClass != other?.javaClass) return false
-		return list == (other as NonEmptyList<*>).list
+	fun <B> flatMap(f: (A) -> NonEmptyList<B>): NonEmptyList<B> {
+		val headList = f(head)
+		return NonEmptyList(headList.head, headList.tail + tail.flatMap(f))
 	}
 
-	override fun hashCode() = list.hashCode()
+	override fun <R> fold(initialValue: R, accumulator: (R, A) -> R): R =
+			tail.fold(accumulator(initialValue, head), accumulator)
+
+	fun plus(other: A) = NonEmptyList(head, tail + other)
+	fun plus(other: Iterable<A>) = NonEmptyList(head, tail + other)
 
 	companion object {
-		fun <T> just(item: T) = NonEmptyList(listOf(item))
+		fun <T> just(item: T) = NonEmptyList(item, listOf(item))
+		fun <T> of(head: T, vararg others: T) = of(head, others.toList())
+		fun <T> of(head: T, others: List<T>) = NonEmptyList(head, others)
 	}
 }
 
 val <A> Context<NonEmptyList<*>, A>.asNel get() = this as NonEmptyList<A>
+
+fun <A> List<A>.nonEmptyOrNull(): NonEmptyList<A>? {
+	return NonEmptyList(
+			firstOrNull() ?: return null,
+			drop(1)
+	)
+}
+
+fun <A> List<A>.concat(other: NonEmptyList<A>) =
+		nonEmptyOrNull()?.plus(other) ?: other
