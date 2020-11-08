972638d5597cce665538596fa28f88b52777e075
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2020-11-08 15:16:58 +0000

    Validation sample: Form validation

---
+++ samples/src/main/kotlin/com/github/fsbarata/functional/samples/FormValidation.kt
@@ -0,0 +1,59 @@
+package com.github.fsbarata.functional.samples
+
+import com.github.fsbarata.functional.data.list.NonEmptyList
+import com.github.fsbarata.functional.data.list.concatNelSemigroup
+import com.github.fsbarata.functional.data.validation.Validation
+import com.github.fsbarata.functional.data.validation.sequence
+import java.util.regex.Pattern
+
+typealias Email = String
+typealias PhoneNumber = String
+
+sealed class ValidationError {
+    object InvalidMail : ValidationError()
+    object InvalidPhoneNumber : ValidationError()
+}
+
+private fun Email.toValidatedEmail(): Validation<NonEmptyList<ValidationError>, Email> =
+    when {
+        isValidEmail() -> Validation.Success(this)
+        else -> Validation.Failure(ValidationError.InvalidMail).toValidationNel()
+    }
+
+private fun PhoneNumber.toValidatedPhoneNumber(): Validation<NonEmptyList<ValidationError>, PhoneNumber> =
+    when {
+        isValidPhoneNumber() -> Validation.Success(this)
+        else -> Validation.Failure(ValidationError.InvalidPhoneNumber).toValidationNel()
+    }
+
+private val EMAIL_ADDRESS: Pattern = Pattern.compile(
+    "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+            "\\@" +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+            "(" +
+            "\\." +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+            ")+"
+)
+
+private fun Email.isValidEmail() = EMAIL_ADDRESS.matcher(this).matches()
+private fun PhoneNumber.isValidPhoneNumber() = all { it.isDigit() }
+
+fun validateData(
+    mail: String,
+    phoneNumber: String
+) = concatNelSemigroup<ValidationError>().sequence(
+    mail.toValidatedEmail(),
+    phoneNumber.toValidatedPhoneNumber(),
+    ::Pair
+)
+
+private fun NonEmptyList<ValidationError>.handleInvalid() = map {
+    handleInvalidField(it)
+}
+
+private fun handleInvalidField(validationError: ValidationError): String =
+    when (validationError) {
+        ValidationError.InvalidMail -> "Invalid email"
+        ValidationError.InvalidPhoneNumber -> "Invalid phone number"
+    }
---
+++ samples/src/test/kotlin/com/github/fsbarata/functional/samples/FormValidationKtTest.kt
@@ -0,0 +1,43 @@
+package com.github.fsbarata.functional.samples
+
+import com.github.fsbarata.functional.data.list.NonEmptyList
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class FormValidationKtTest {
+
+    @Test
+    fun `validation accumulates errors`() {
+        var actual: NonEmptyList<ValidationError>? = null
+        val expected = NonEmptyList.of(ValidationError.InvalidMail, ValidationError.InvalidPhoneNumber)
+        validateData("a@", "123-").fold(
+            ifFailure = { actual = it },
+            ifSuccess = { throw Exception() })
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `validation goes towards ifFailure if one of the validation fails`() {
+        var actual: NonEmptyList<ValidationError>? = null
+        val expected = NonEmptyList.of(ValidationError.InvalidMail)
+        validateData("a@", "123").fold(
+            ifFailure = { actual = it },
+            ifSuccess = { throw Exception() })
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `validation goes towards ifSuccess if both elements are valid`() {
+        var actual: Pair<String, String>? = null
+        val mail = "a@gmail.com"
+        val phoneNumber = "123"
+        val expected = mail to phoneNumber
+        validateData(mail, phoneNumber).fold(
+            ifFailure = { throw Exception() },
+            ifSuccess = { actual = it })
+
+        assertEquals(expected, actual)
+    }
+}
