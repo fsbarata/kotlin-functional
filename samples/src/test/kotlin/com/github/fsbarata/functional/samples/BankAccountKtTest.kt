f3ee91bd8a369babb424aadd927a4c93bd5401e2
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2020-11-08 17:03:06 +0000

    Import codestyle to use tabs

--- samples/src/test/kotlin/com/github/fsbarata/functional/samples/BankAccountKtTest.kt
+++ samples/src/test/kotlin/com/github/fsbarata/functional/samples/BankAccountKtTest.kt
@@ -7,22 +7,22 @@ import java.math.BigDecimal
 
 class BankAccountKtTest {
 
-    @Test
-    fun `transfer half the money from one account to other`() {
-        val bank = Bank(
-            name = "Central Bank",
-            swiftCode = "CBESMMX",
-            accounts = listOf(
-                Account(fromAccountId, BigDecimal("200")),
-                Account(toAccountId, BigDecimal("0"))
-            )
-        )
-        val transferred = transferHalfOfTheMoney(from = fromAccountId, to = toAccountId).runState(bank).second
-        assertThat(transferred, Matchers.comparesEqualTo(BigDecimal("100")))
-    }
+	@Test
+	fun `transfer half the money from one account to other`() {
+		val bank = Bank(
+				name = "Central Bank",
+				swiftCode = "CBESMMX",
+				accounts = listOf(
+						Account(fromAccountId, BigDecimal("200")),
+						Account(toAccountId, BigDecimal("0"))
+				)
+		)
+		val transferred = transferHalfOfTheMoney(from = fromAccountId, to = toAccountId).runState(bank).second
+		assertThat(transferred, Matchers.comparesEqualTo(BigDecimal("100")))
+	}
 
-    companion object {
-        const val fromAccountId = "101931"
-        const val toAccountId = "101932"
-    }
+	companion object {
+		const val fromAccountId = "101931"
+		const val toAccountId = "101932"
+	}
 }
--- samples/src/test/kotlin/com/github/fsbarata/functional/samples/FormValidationKtTest.kt
+++ samples/src/test/kotlin/com/github/fsbarata/functional/samples/FormValidationKtTest.kt
@@ -6,38 +6,38 @@ import org.junit.Test
 
 class FormValidationKtTest {
 
-    @Test
-    fun `validation accumulates errors`() {
-        var actual: NonEmptyList<ValidationError>? = null
-        val expected = NonEmptyList.of(ValidationError.InvalidMail, ValidationError.InvalidPhoneNumber)
-        validateData("a@", "123-").fold(
-            ifFailure = { actual = it },
-            ifSuccess = { throw Exception() })
+	@Test
+	fun `validation accumulates errors`() {
+		var actual: NonEmptyList<ValidationError>? = null
+		val expected = NonEmptyList.of(ValidationError.InvalidMail, ValidationError.InvalidPhoneNumber)
+		validateData("a@", "123-").fold(
+				ifFailure = { actual = it },
+				ifSuccess = { throw Exception() })
 
-        assertEquals(expected, actual)
-    }
+		assertEquals(expected, actual)
+	}
 
-    @Test
-    fun `validation goes towards ifFailure if one of the validation fails`() {
-        var actual: NonEmptyList<ValidationError>? = null
-        val expected = NonEmptyList.of(ValidationError.InvalidMail)
-        validateData("a@", "123").fold(
-            ifFailure = { actual = it },
-            ifSuccess = { throw Exception() })
+	@Test
+	fun `validation goes towards ifFailure if one of the validation fails`() {
+		var actual: NonEmptyList<ValidationError>? = null
+		val expected = NonEmptyList.of(ValidationError.InvalidMail)
+		validateData("a@", "123").fold(
+				ifFailure = { actual = it },
+				ifSuccess = { throw Exception() })
 
-        assertEquals(expected, actual)
-    }
+		assertEquals(expected, actual)
+	}
 
-    @Test
-    fun `validation goes towards ifSuccess if both elements are valid`() {
-        var actual: Pair<String, String>? = null
-        val mail = "a@gmail.com"
-        val phoneNumber = "123"
-        val expected = mail to phoneNumber
-        validateData(mail, phoneNumber).fold(
-            ifFailure = { throw Exception() },
-            ifSuccess = { actual = it })
+	@Test
+	fun `validation goes towards ifSuccess if both elements are valid`() {
+		var actual: Pair<String, String>? = null
+		val mail = "a@gmail.com"
+		val phoneNumber = "123"
+		val expected = mail to phoneNumber
+		validateData(mail, phoneNumber).fold(
+				ifFailure = { throw Exception() },
+				ifSuccess = { actual = it })
 
-        assertEquals(expected, actual)
-    }
+		assertEquals(expected, actual)
+	}
 }
