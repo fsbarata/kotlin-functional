f75bb4e12d851d9724a55b7e317b0505c70157e9
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2020-11-08 15:16:45 +0000

    State Monad sample: Bank account

---
+++ samples/src/main/kotlin/com/github/fsbarata/functional/samples/BankAccount.kt
@@ -0,0 +1,41 @@
+package com.github.fsbarata.functional.samples
+
+import com.github.fsbarata.functional.control.monad.state.State
+import java.math.BigDecimal
+
+typealias Balance = BigDecimal
+typealias AccountId = String
+
+internal data class Account(val id: AccountId, var balance: Balance)
+internal data class Bank(val name: String, val swiftCode: String, val accounts: List<Account> = emptyList())
+
+private fun getAccountBalance(accountId: AccountId) = State<Bank, Balance> { bank ->
+    Pair(bank, bank.accounts.find { it.id == accountId }?.balance ?: BigDecimal.ZERO)
+}
+
+private fun withdraw(accountId: AccountId, amount: BigDecimal) = State<Bank, Balance> { bank ->
+    bank.copy(accounts = bank.accounts.map { acc ->
+        if (acc.id == accountId && acc.balance >= amount) {
+            acc.copy(balance = acc.balance - amount)
+        } else {
+            acc
+        }
+    }) to amount
+}
+
+private fun deposit(accountId: AccountId, amount: BigDecimal) = State<Bank, Balance> { bank ->
+    bank.copy(accounts = bank.accounts.map { acc ->
+        if (acc.id == accountId) {
+            acc.copy(balance = acc.balance + amount)
+        } else {
+            acc
+        }
+    }) to amount
+}
+
+internal fun transferHalfOfTheMoney(from: AccountId, to: AccountId): State<Bank, Balance> =
+    getAccountBalance(from).bind { accountBalance ->
+        withdraw(from, accountBalance.multiply(BigDecimal("0.5"))).bind { withdrawn ->
+            deposit(to, withdrawn)
+        }
+    }
---
+++ samples/src/test/kotlin/com/github/fsbarata/functional/samples/BankAccountKtTest.kt
@@ -0,0 +1,28 @@
+package com.github.fsbarata.functional.samples
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
+import org.junit.Test
+import java.math.BigDecimal
+
+class BankAccountKtTest {
+
+    @Test
+    fun `transfer half the money from one account to other`() {
+        val bank = Bank(
+            name = "Central Bank",
+            swiftCode = "CBESMMX",
+            accounts = listOf(
+                Account(fromAccountId, BigDecimal("200")),
+                Account(toAccountId, BigDecimal("0"))
+            )
+        )
+        val transferred = transferHalfOfTheMoney(from = fromAccountId, to = toAccountId).runState(bank).second
+        assertThat(transferred, Matchers.comparesEqualTo(BigDecimal("100")))
+    }
+
+    companion object {
+        const val fromAccountId = "101931"
+        const val toAccountId = "101932"
+    }
+}
