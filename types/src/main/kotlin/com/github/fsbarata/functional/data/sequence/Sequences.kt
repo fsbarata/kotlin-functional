0d63e7ddc6fdde926fb8b75a6130e77c063ffd99
fixedlight <fixedlight@gmail.com>
2020-11-10 06:55:09 +0000

    TreeSequence type, same as Tree but with a Sequence forest

--- types/src/main/kotlin/com/github/fsbarata/functional/data/sequence/Sequences.kt
+++ types/src/main/kotlin/com/github/fsbarata/functional/data/sequence/Sequences.kt
@@ -1,26 +1,27 @@
-package com.github.fsbarata.functional.data.list
+package com.github.fsbarata.functional.data.sequence
 
 import com.github.fsbarata.functional.control.Applicative
-import com.github.fsbarata.functional.data.partial
+import com.github.fsbarata.functional.data.*
 
 /**
- * Extensions to kotlin List, without needing to wrap in ListF
- *
- * Tested by ListFTest
+ * Extensions to kotlin Sequence
  */
 
 @Suppress("NOTHING_TO_INLINE")
-inline fun <A, B> List<A>.ap(fs: List<(A) -> B>): List<B> =
+inline fun <A, B> Sequence<A>.ap(fs: Sequence<(A) -> B>): Sequence<B> =
 	flatMap { a -> fs.map { f -> f(a) } }
 
-inline fun <A, B, C> List<A>.lift2(lb: List<B>, f: (A, B) -> C): List<C> =
+fun <A, B, C> Sequence<A>.lift2(lb: Sequence<B>, f: (A, B) -> C): Sequence<C> =
 	flatMap { a -> lb.map(f.partial(a)) }
 
-inline fun <F, A, B> List<A>.traverse(
+fun <F, A, B> Sequence<A>.traverse(
 	appScope: Applicative.Scope<F>,
 	f: (A) -> Applicative<F, B>,
-): Applicative<F, List<B>> {
-	return fold(appScope.just(emptyList())) { app, a ->
+): Applicative<F, Sequence<B>> {
+	return fold(appScope.just(emptySequence())) { app, a ->
 		f(a).lift2(app) { b, lb -> lb + b }
 	}
 }
+
+fun <M, A> Sequence<A>.foldMap(monoid: Monoid<M>, f: (A) -> M): M =
+	fold(monoid.empty, (monoid.dual()::combine compose f).flip())
---
+++ types/src/main/kotlin/com/github/fsbarata/functional/data/tree/TreeSequence.kt
@@ -0,0 +1,79 @@
+package com.github.fsbarata.functional.data.tree
+
+import com.github.fsbarata.functional.control.Applicative
+import com.github.fsbarata.functional.control.Comonad
+import com.github.fsbarata.functional.control.Context
+import com.github.fsbarata.functional.control.Monad
+import com.github.fsbarata.functional.data.Monoid
+import com.github.fsbarata.functional.data.Traversable
+import com.github.fsbarata.functional.data.partial
+import com.github.fsbarata.functional.data.sequence.NonEmptySequenceBase
+import com.github.fsbarata.functional.data.sequence.foldMap
+import com.github.fsbarata.functional.data.sequence.traverse
+import com.github.fsbarata.functional.iterators.NonEmptyIterator
+
+typealias ForestSequence<A> = Sequence<TreeSequence<A>>
+
+internal typealias TreeSequenceContext = TreeSequence<*>
+
+@Suppress("OVERRIDE_BY_INLINE")
+data class TreeSequence<out A>(
+	val root: A,
+	val sub: ForestSequence<A> = emptySequence(),
+): Monad<TreeSequenceContext, A>,
+	Traversable<TreeSequenceContext, A>,
+	Comonad<TreeSequenceContext, A>,
+	NonEmptySequenceBase<A> {
+	override val scope = TreeSequence
+
+	override fun iterator() = NonEmptyIterator(
+		root,
+		sub.flatten().iterator()
+	)
+
+	override fun extract() = root
+
+	override fun <B> bind(f: (A) -> Context<TreeSequenceContext, B>) = flatMap { f(it).asTreeSequence }
+
+	fun <B> flatMap(f: (A) -> TreeSequence<B>): TreeSequence<B> {
+		val newTree = f(root)
+		return TreeSequence(
+			newTree.root,
+			newTree.sub + sub.flatten().map(f)
+		)
+	}
+
+	override fun <B, R> lift2(fb: Applicative<TreeSequenceContext, B>, f: (A, B) -> R): TreeSequence<R> {
+		val tb = fb.asTreeSequence
+		val x: ForestSequence<R> = tb.sub.map { it.map(f.partial(root)) }
+		val y: ForestSequence<R> = sub.map { it.lift2(tb, f) }
+		return TreeSequence(
+			f(root, tb.root),
+			x + y
+		)
+	}
+
+	override fun <B> map(f: (A) -> B): TreeSequence<B> =
+		TreeSequence(f(root), sub.map { it.map(f) })
+
+	override fun <M> foldMap(monoid: Monoid<M>, f: (A) -> M): M =
+		monoid.combine(f(root), sub.foldMap(monoid) { ta -> ta.foldMap(monoid, f) })
+
+	override fun <F, B> traverse(
+		appScope: Applicative.Scope<F>,
+		f: (A) -> Applicative<F, B>,
+	): Applicative<F, TreeSequence<B>> =
+		f(root).lift2(sub.traverse(appScope) { it.traverse(appScope, f) }, ::TreeSequence)
+
+	override fun duplicate(): TreeSequence<TreeSequence<A>> =
+		TreeSequence(this, sub.map { it.duplicate() })
+
+	companion object
+		: Monad.Scope<TreeSequenceContext>,
+		Traversable.Scope<TreeSequenceContext> {
+		override fun <A> just(a: A) = TreeSequence(a)
+	}
+}
+
+val <A> Context<TreeSequenceContext, A>.asTreeSequence
+	get() = this as TreeSequence<A>
