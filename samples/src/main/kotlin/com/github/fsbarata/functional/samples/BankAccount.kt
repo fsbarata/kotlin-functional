13ebf1f88db754011453d898ad4ce7085feccda7
fixedlight <fixedlight@gmail.com>
2020-11-12 21:21:43 +0000

    added modify and gets to State monad

--- samples/src/main/kotlin/com/github/fsbarata/functional/samples/BankAccount.kt
+++ samples/src/main/kotlin/com/github/fsbarata/functional/samples/BankAccount.kt
@@ -1,37 +1,38 @@
 package com.github.fsbarata.functional.samples
 
 import com.github.fsbarata.functional.control.monad.state.State
+import com.github.fsbarata.functional.data.tuple.Tuple2
 import java.math.BigDecimal
 
 typealias Balance = BigDecimal
 typealias AccountId = String
 
-internal data class Account(val id: AccountId, var balance: Balance)
+internal data class Account(val id: AccountId, val balance: Balance)
 internal data class Bank(val name: String, val swiftCode: String, val accounts: List<Account> = emptyList())
 
-private fun getAccountBalance(accountId: AccountId) = State<Bank, Balance> { bank ->
-	Pair(bank, bank.accounts.find { it.id == accountId }?.balance ?: BigDecimal.ZERO)
+private fun getAccountBalance(accountId: AccountId) = State.gets<Bank, Balance> { bank ->
+	bank.accounts.find { it.id == accountId }?.balance ?: BigDecimal.ZERO
 }
 
-private fun withdraw(accountId: AccountId, amount: BigDecimal) = State<Bank, Balance> { bank ->
+private fun withdraw(accountId: AccountId, amount: BigDecimal) = State.modify<Bank> { bank ->
 	bank.copy(accounts = bank.accounts.map { acc ->
 		if (acc.id == accountId && acc.balance >= amount) {
 			acc.copy(balance = acc.balance - amount)
 		} else {
 			acc
 		}
-	}) to amount
-}
+	})
+}.map { amount }
 
-private fun deposit(accountId: AccountId, amount: BigDecimal) = State<Bank, Balance> { bank ->
+private fun deposit(accountId: AccountId, amount: BigDecimal) = State.modify<Bank> { bank ->
 	bank.copy(accounts = bank.accounts.map { acc ->
 		if (acc.id == accountId) {
 			acc.copy(balance = acc.balance + amount)
 		} else {
 			acc
 		}
-	}) to amount
-}
+	})
+}.map { amount }
 
 internal fun transferHalfOfTheMoney(from: AccountId, to: AccountId): State<Bank, Balance> =
 	getAccountBalance(from).bind { accountBalance ->
--- types/src/main/kotlin/com/github/fsbarata/functional/control/monad/state/State.kt
+++ types/src/main/kotlin/com/github/fsbarata/functional/control/monad/state/State.kt
@@ -2,39 +2,38 @@ package com.github.fsbarata.functional.control.monad.state
 
 import com.github.fsbarata.functional.control.Context
 import com.github.fsbarata.functional.control.Monad
+import com.github.fsbarata.functional.data.tuple.Tuple2
 
 class State<S, A>(
-	val runState: (S) -> Pair<S, A>,
+	val runState: (S) -> Tuple2<S, A>,
 ): Monad<StateContext<S>, A> {
 	override val scope get() = StateScope<S>()
 
 	override fun <B> map(f: (A) -> B): State<S, B> =
-		State { s ->
-			val (newState, value) = runState(s)
-			Pair(newState, f(value))
-		}
+		State { s -> runState(s).map(f) }
 
 	override infix fun <B> bind(f: (A) -> Context<StateContext<S>, B>): State<S, B> =
+		flatMap { f(it).asState }
+
+	fun <B> flatMap(f: (A) -> State<S, B>): State<S, B> =
 		State { s ->
 			val (newState, value) = runState(s)
-			f(value).asState.runState(newState)
+			f(value).runState(newState)
 		}
 
 	class StateScope<S>: Monad.Scope<StateContext<S>> {
 		override fun <A> just(a: A) = just<S, A>(a)
 
-		fun get() = State { s: S -> Pair(s, s) }
-		fun put(newState: S) = State<S, Unit> { Pair(newState, Unit) }
+		fun get() = State { s: S -> Tuple2(s, s) }
+		fun put(newState: S) = State<S, Unit> { Tuple2(newState, Unit) }
 	}
 
 	companion object {
-		fun <S, A> just(a: A) = State<S, A> { s -> Pair(s, a) }
+		fun <S, A> just(a: A) = State<S, A> { s -> Tuple2(s, a) }
+		fun <S> modify(f: (S) -> S) = State<S, Unit> { s -> Tuple2(f(s), Unit) }
+		fun <S, A> gets(f: (S) -> A) = State<S, A> { s -> Tuple2(s, f(s)) }
 	}
 }
-
-
-fun <S> getState(): State<S, S> = State { s -> Pair(s, s) }
-fun <S> putState(newState: S) = State<S, Unit> { Pair(newState, Unit) }
 
 internal typealias StateContext<S> = State<S, *>
 
