87ff1fdf7dd4a31d8be1477d61b15dc49db85b01
fixedlight <fixedlight@gmail.com>
2020-11-14 08:10:56 +0000

    reader tests

--- types/src/main/kotlin/com/github/fsbarata/functional/control/monad/reader/Reader.kt
+++ types/src/main/kotlin/com/github/fsbarata/functional/control/monad/reader/Reader.kt
@@ -3,31 +3,32 @@ package com.github.fsbarata.functional.control.monad.reader
 import com.github.fsbarata.functional.control.Applicative
 import com.github.fsbarata.functional.Context
 import com.github.fsbarata.functional.control.Monad
+import com.github.fsbarata.functional.data.id
 
 /**
  * Reader Monad
  *
  * This object models a set of dependencies D, required to generate the value A.
  */
-class Reader<D, out A>(val run: (D) -> A): Monad<ReaderContext<D>, A> {
+class Reader<D, out A>(val runReader: (D) -> A): Monad<ReaderContext<D>, A> {
 	override val scope get() = ReaderScope<D>()
 
 	override fun <B> map(f: (A) -> B): Reader<D, B> =
-		Reader { f(run(it)) }
+		Reader { f(runReader(it)) }
 
 	override infix fun <B> ap(ff: Applicative<ReaderContext<D>, (A) -> B>): Reader<D, B> =
-		Reader { d -> ff.map { it(run(d)) }.asReader.run(d) }
+		Reader { d -> ff.map { it(runReader(d)) }.asReader.runReader(d) }
 
 	override infix fun <B> bind(f: (A) -> Context<ReaderContext<D>, B>): Reader<D, B> =
 		flatMap { f(it).asReader }
 
 	fun <B> flatMap(f: (A) -> Reader<in D, B>) =
-		Reader<D, B> { f(run(it)).run(it) }
+		Reader<D, B> { f(runReader(it)).runReader(it) }
 
-	fun <E> leftMap(f: (E) -> D): Reader<E, A> =
-		Reader { e -> run(f(e)) }
+	fun <E> using(f: (E) -> D): Reader<E, A> =
+		Reader { e -> runReader(f(e)) }
 
-	operator fun invoke(d: D) = run(d)
+	operator fun invoke(d: D) = runReader(d)
 
 	class ReaderScope<D>: Monad.Scope<ReaderContext<D>> {
 		override fun <A> just(a: A) = just<D, A>(a)
@@ -36,7 +37,7 @@ class Reader<D, out A>(val run: (D) -> A): Monad<ReaderContext<D>, A> {
 	companion object {
 		fun <D, A> just(a: A): Reader<D, A> = Reader { a }
 
-		fun <D> ask(): Reader<D, D> = Reader { it }
+		fun <D> ask(): Reader<D, D> = Reader(::id)
 	}
 }
 
--- types/src/test/kotlin/com/github/fsbarata/functional/control/monad/reader/ReaderTest.kt
+++ types/src/test/kotlin/com/github/fsbarata/functional/control/monad/reader/ReaderTest.kt
@@ -2,6 +2,9 @@ package com.github.fsbarata.functional.control.monad.reader
 
 import com.github.fsbarata.functional.control.test.MonadLaws
 import com.github.fsbarata.functional.data.Functor
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import kotlin.math.roundToInt
 
 private typealias Dependencies = Pair<String, Int>
 
@@ -12,5 +15,36 @@ class ReaderTest:
 	private val basicDependencies = Pair("ab", -38)
 
 	override fun <A> Functor<ReaderContext<Dependencies>, A>.equalTo(other: Functor<ReaderContext<Dependencies>, A>) =
-		asReader.run(basicDependencies) == other.asReader.run(basicDependencies)
+		asReader.runReader(basicDependencies) == other.asReader.runReader(basicDependencies)
+
+	@Test
+	fun map() {
+		val reader = Reader { a: Int -> (a + 2).toString() + 2 }
+		assertEquals("52a", reader.map { it + "a" }.runReader(3))
+	}
+
+	@Test
+	fun flatMap() {
+		val reader = Reader { a: Int -> (a + 2).toString() + 2 }
+		assertEquals(
+			"62a4",
+			reader.flatMap { b ->
+				Reader { a: Int -> b + "a$a" }
+			}.runReader(4)
+		)
+	}
+
+	@Test
+	fun using() {
+		val reader = Reader { a: Int -> (a + 2).toString() + 2 }
+		assertEquals(
+			"52",
+			reader.using { b: Double -> (b * 0.3).roundToInt() }.runReader(8.5)
+		)
+	}
+
+	@Test
+	fun ask() {
+		assertEquals("11", Reader.ask<String>().runReader("11"))
+	}
 }
