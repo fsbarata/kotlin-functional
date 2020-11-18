6bc62137d2f60ec50a5a69d1920308c13933958c
fixedlight <fixedlight@gmail.com>
2020-11-18 19:16:45 +0000

    added plus element nes to sequences

--- types/src/main/kotlin/com/github/fsbarata/functional/kotlin/NesExtensions.kt
+++ types/src/main/kotlin/com/github/fsbarata/functional/kotlin/NesExtensions.kt
@@ -1,7 +1,14 @@
 package com.github.fsbarata.functional.kotlin
 
 import com.github.fsbarata.functional.data.sequence.NonEmptySequence
+import com.github.fsbarata.functional.data.sequence.nonEmpty
 import com.github.fsbarata.functional.utils.NonEmptyIterator
+
+fun <A> Sequence<A>.plusElementNes(item: A): NonEmptySequence<A> =
+	plusElement(item).nonEmpty { throw NoSuchElementException() }
+
+fun <A> Sequence<A>.plusNes(other: NonEmptySequence<A>): NonEmptySequence<A> =
+	plus(other).nonEmpty { throw NoSuchElementException() }
 
 fun <A, R> Sequence<A>.scanNes(initialValue: R, operation: (R, A) -> R): NonEmptySequence<R> {
 	val scan = scan(initialValue, operation)
