9e482546eaa86d15b2d4d2fca2e9dcfb7036c901
fixed <fixedlight@gmail.com>
2020-11-20 11:23:37 +0000

    Validation lift extensions

--- types/src/main/kotlin/com/github/fsbarata/functional/data/validation/ValidationApplicative.kt
+++ types/src/main/kotlin/com/github/fsbarata/functional/data/validation/ValidationApplicative.kt
@@ -1,6 +1,9 @@
 package com.github.fsbarata.functional.data.validation
 
 import com.github.fsbarata.functional.control.Applicative
+import com.github.fsbarata.functional.control.Lift2
+import com.github.fsbarata.functional.control.Lift3
+import com.github.fsbarata.functional.control.Lift4
 import com.github.fsbarata.functional.data.Semigroup
 import com.github.fsbarata.functional.data.partial
 
@@ -56,3 +59,21 @@ inline fun <E: Semigroup<E>, A, B, C, D, R> lift4(
 	fa: Validation<E, A>, fb: Validation<E, B>, fc: Validation<E, C>, fd: Validation<E, D>,
 	f: (A, B, C, D) -> R,
 ): Validation<E, R> = fd.ap(lift3(fa, fb, fc) { a, b, c -> f.partial(a, b, c) })
+
+operator fun <E: Semigroup<E>, A, B, R> Lift2<A, B, R>.invoke(
+	v1: Validation<E, A>,
+	v2: Validation<E, B>
+) = lift2(v1, v2, f)
+
+operator fun <E: Semigroup<E>, A, B, C, R> Lift3<A, B, C, R>.invoke(
+	v1: Validation<E, A>,
+	v2: Validation<E, B>,
+	v3: Validation<E, C>,
+) = lift3(v1, v2, v3, f)
+
+operator fun <E: Semigroup<E>, A, B, C, D, R> Lift4<A, B, C, D, R>.invoke(
+	v1: Validation<E, A>,
+	v2: Validation<E, B>,
+	v3: Validation<E, C>,
+	v4: Validation<E, D>,
+) = lift4(v1, v2, v3, v4, f)
