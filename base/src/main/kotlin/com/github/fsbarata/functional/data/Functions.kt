13e9a9f68b344e9bf37358658b72023c758be1dd
fixedlight <fixedlight@gmail.com>
2020-11-21 17:21:31 +0000

    added curried flip

--- base/src/main/kotlin/com/github/fsbarata/functional/data/Functions.kt
+++ base/src/main/kotlin/com/github/fsbarata/functional/data/Functions.kt
@@ -61,6 +61,9 @@ inline fun <A, B, C, R> ((A, B) -> (C) -> R).uncurry(): F3<A, B, C, R> =
 @Suppress("NOTHING_TO_INLINE")
 inline fun <A, B, R> F2<A, B, R>.flip(): F2<B, A, R> = { b, a -> invoke(a, b) }
 
+@Suppress("NOTHING_TO_INLINE")
+inline fun <A, B, R> ((A) -> (B) -> R).flip(): (B) -> (A) -> R = { b -> { a -> invoke(a).invoke(b) } }
+
 fun <A, B, R> on(f1: F2<B, B, R>, f2: F1<A, B>): (A, A) -> R = { a1, a2 -> f1(f2(a1), f2(a2)) }
 fun <A, B, R> on(f1: (B) -> (B) -> R, f2: F1<A, B>): (A) -> (A) -> R =
 	{ a1 ->
