8b0e367527ca2649a8e169f117133051761383ff
fixedlight <fixedlight@gmail.com>
2020-11-25 19:21:10 +0000

    completed Kliesli tests

--- base/src/test/kotlin/com/github/fsbarata/functional/control/arrow/KleisliTest.kt
+++ base/src/test/kotlin/com/github/fsbarata/functional/control/arrow/KleisliTest.kt
@@ -13,4 +13,36 @@ class KleisliTest {
 		val k = Optional.kleisli(f) compose (Optional.kleisli(g))
 		assertEquals(Optional.just("3.5"), m.bind(k))
 	}
+
+	@Test
+	fun first() {
+		val pass = IllegalStateException()
+		val f = { b: Int -> Optional.just("$b") }
+		val k = Optional.kleisli(f).first<Exception>()
+		assertEquals(Optional.just(Pair("3", pass)), Optional.just(Pair(3, pass)).bind(k))
+	}
+
+	@Test
+	fun second() {
+		val pass = IllegalStateException()
+		val f = { b: Int -> Optional.just("$b") }
+		val k = Optional.kleisli(f).second<Exception>()
+		assertEquals(Optional.just(Pair(pass, "3")), Optional.just(Pair(pass, 3)).bind(k))
+	}
+
+	@Test
+	fun split() {
+		val f = { b: Int -> Optional.just("$b") }
+		val g = { a: Long -> Optional.just(a + 0.5) }
+		val k = Optional.kleisli(f) split Optional.kleisli(g)
+		assertEquals(Optional.just(Pair("3", 3.5)), Optional.just(Pair(3, 3L)).bind(k))
+	}
+
+	@Test
+	fun fanout() {
+		val f = { b: Int -> Optional.just("$b") }
+		val g = { a: Int -> Optional.just(a + 0.5) }
+		val k = Optional.kleisli(f) fanout Optional.kleisli(g)
+		assertEquals(Optional.just(Pair("3", 3.5)), Optional.just(3).bind(k))
+	}
 }
