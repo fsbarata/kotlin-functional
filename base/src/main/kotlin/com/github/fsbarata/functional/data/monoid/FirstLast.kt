155b4787ee11802b8dc568653d5ee4c7882e75b8
fixedlight <fixedlight@gmail.com>
2021-03-27 19:34:52 +0000

    differentiating between First and FirstNotNull semigroup

--- base/src/main/kotlin/com/github/fsbarata/functional/data/monoid/FirstLast.kt
+++ base/src/main/kotlin/com/github/fsbarata/functional/data/monoid/FirstLast.kt
@@ -4,11 +4,15 @@ import com.github.fsbarata.functional.data.Monoid
 import com.github.fsbarata.functional.data.Semigroup
 import com.github.fsbarata.functional.data.monoid
 
-class First<A>(val get: A?): Semigroup<First<A>> {
-	override fun combineWith(other: First<A>) = First(get ?: other.get)
+class First<A: Any>(val get: A): Semigroup<First<A>> {
+	override fun combineWith(other: First<A>) = First(get)
+}
+
+class FirstNotNull<A>(val get: A): Semigroup<FirstNotNull<A>> {
+	override fun combineWith(other: FirstNotNull<A>) = FirstNotNull(get ?: other.get)
 
 	companion object {
-		fun <A> monoid() = monoid(First<A>(null))
+		fun <A: Any> monoid() = monoid(FirstNotNull<A?>(null))
 	}
 }
 
@@ -18,11 +22,15 @@ fun <A> firstMonoid() = object: Monoid<A?> {
 	override fun combine(a1: A?, a2: A?): A? = a1 ?: a2
 }
 
-class Last<A>(val get: A?): Semigroup<Last<A>> {
-	override fun combineWith(other: Last<A>) = Last(other.get ?: get)
+class Last<A: Any>(val get: A): Semigroup<Last<A>> {
+	override fun combineWith(other: Last<A>) = Last(other.get)
+}
+
+class LastNotNull<A>(val get: A): Semigroup<LastNotNull<A>> {
+	override fun combineWith(other: LastNotNull<A>) = LastNotNull(other.get ?: get)
 
 	companion object {
-		fun <A> monoid() = monoid(Last<A>(null))
+		fun <A: Any> monoid() = monoid(LastNotNull<A?>(null))
 	}
 }
 
