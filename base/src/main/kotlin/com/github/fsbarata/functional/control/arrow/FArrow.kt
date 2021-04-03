b595f2d3a53d6f251d96d4dcbd73feec9b97b800
fixedlight <fixedlight@gmail.com>
2021-04-03 08:20:30 +0100

    converted fapp to extension function

--- base/src/main/kotlin/com/github/fsbarata/functional/control/arrow/FArrow.kt
+++ base/src/main/kotlin/com/github/fsbarata/functional/control/arrow/FArrow.kt
@@ -24,4 +24,4 @@ inline infix fun <A, R, B, RR> F1<A, R>.splitChoice(crossinline other: F1<B, RR>
 inline infix fun <A, B, R> F1<A, R>.fanin(crossinline other: F1<B, R>): F1<Either<A, B>, R> =
 	{ it.fold(this, other) }
 
-inline fun <A, R> fapp(): F1<Pair<F1<A, R>, A>, R> = { (f, a) -> f(a) }
+inline fun <A, R> Pair<F1<A, R>, A>.fapp(): R = first.invoke(second)
