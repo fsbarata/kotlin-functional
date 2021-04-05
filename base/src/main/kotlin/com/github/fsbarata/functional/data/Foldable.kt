3a30929845240fbd630fa8f329964967984bcad8
fixedlight <fixedlight@gmail.com>
2021-04-05 16:40:50 +0100

    foldable foldL, foldR, scanL, scanR variants

--- base/src/main/kotlin/com/github/fsbarata/functional/data/Foldable.kt
+++ base/src/main/kotlin/com/github/fsbarata/functional/data/Foldable.kt
@@ -3,6 +3,7 @@ package com.github.fsbarata.functional.data
 import com.github.fsbarata.functional.control.Alternative
 import com.github.fsbarata.functional.data.list.ListF
 import com.github.fsbarata.functional.data.list.NonEmptyList
+import com.github.fsbarata.functional.data.sequence.NonEmptySequence
 
 /**
  * Foldable structure
@@ -37,15 +38,26 @@ interface Foldable<out A> {
 		foldL(monoid.empty) { r, a -> monoid.combine(r, f(a)) }
 }
 
-fun <A: Semigroup<A>> Foldable<A>.fold(monoid: Monoid<A>) = foldMap(monoid, id())
-
 fun <A, R> Foldable<A>.scanL(initialValue: R, accumulator: (R, A) -> R): NonEmptyList<R> =
-	foldL(NonEmptyList.just(initialValue)) { nel, v ->
-		nel + accumulator(nel.last(), v)
-	}
+	foldL(Pair(initialValue, NonEmptySequence.just(initialValue))) { (carry, nes), v ->
+		val newValue = accumulator(carry, v)
+		Pair(newValue, nes + newValue)
+	}.second
+		.toList()
 
-fun <A: Semigroup<A>> Foldable<A>.scan(initialValue: A): NonEmptyList<A> =
-	scanL(initialValue, ::combine)
+fun <A, R> Foldable<A>.scanR(initialValue: R, accumulator: (A, R) -> R): NonEmptyList<R> =
+	foldR(Pair(initialValue, NonEmptySequence.just(initialValue))) { v, (carry, nes) ->
+		val newValue = accumulator(v, carry)
+		Pair(newValue, nes + newValue)
+	}.second
+		.toList()
+
+fun <A> Foldable<A>.fold(monoid: Monoid<A>) = foldMap(monoid, id())
+fun <A: Semigroup<A>> Foldable<A>.foldL(initialValue: A) = foldL(initialValue, ::combine)
+fun <A: Semigroup<A>> Foldable<A>.foldR(initialValue: A) = foldR(initialValue, ::combine)
+fun <A: Semigroup<A>> Foldable<A>.scanL(initialValue: A): NonEmptyList<A> = scanL(initialValue, ::combine)
+fun <A: Semigroup<A>> Foldable<A>.scanR(initialValue: A): NonEmptyList<A> = scanR(initialValue, ::combine)
+
 
 fun <A> Foldable<A>.toList(): ListF<A> = foldMap(ListF.monoid()) { ListF.just(it) }
 
