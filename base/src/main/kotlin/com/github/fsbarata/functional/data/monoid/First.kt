a94105f23c8429e8a98f60dd65786355e1b74cc6
fixedlight <fixedlight@gmail.com>
2021-04-05 13:30:55 +0100

    split firstlast into first and last files

--- base/src/main/kotlin/com/github/fsbarata/functional/data/monoid/First.kt
+++ base/src/main/kotlin/com/github/fsbarata/functional/data/monoid/First.kt
@@ -21,21 +21,3 @@ fun <A> firstMonoid() = object: Monoid<A?> {
 
 	override fun combine(a1: A?, a2: A?): A? = a1 ?: a2
 }
-
-data class Last<A: Any>(val get: A): Semigroup<Last<A>> {
-	override fun combineWith(other: Last<A>) = Last(other.get)
-}
-
-data class LastNotNull<A>(val get: A): Semigroup<LastNotNull<A>> {
-	override fun combineWith(other: LastNotNull<A>) = LastNotNull(other.get ?: get)
-
-	companion object {
-		fun <A: Any> monoid(): Monoid<LastNotNull<A?>> = monoid(LastNotNull(null))
-	}
-}
-
-fun <A> lastMonoid() = object: Monoid<A?> {
-	override val empty: A? = null
-
-	override fun combine(a1: A?, a2: A?): A? = a2 ?: a1
-}
