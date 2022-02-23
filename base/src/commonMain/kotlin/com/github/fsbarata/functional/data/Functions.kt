08878f4d69d1db3332995458f94efa946feb0526
Fabio Barata <fixedlight@gmail.com>
2022-02-23 18:27:43 +0000

    more partial implementations for F0 variants

--- base/src/commonMain/kotlin/com/github/fsbarata/functional/data/Functions.kt
+++ base/src/commonMain/kotlin/com/github/fsbarata/functional/data/Functions.kt
@@ -24,21 +24,27 @@ inline infix fun <A, B, C, R> F2<B, C, R>.compose(crossinline other: F1<A, B>): 
 inline infix fun <A, B, C, R> F1<C, R>.compose(crossinline other: F2<A, B, C>): F2<A, B, R> =
 	{ a, b -> invoke(other(a, b)) }
 
+inline infix fun <A, R> F0<A>.composeForward(crossinline other: F1<A, R>): F0<R> = other compose this
 inline infix fun <A, B, R> F1<A, B>.composeForward(crossinline other: F1<B, R>): F1<A, R> = other compose this
 inline infix fun <A, B, C, R> F1<A, B>.composeForward(crossinline other: F2<B, C, R>): F2<A, C, R> = other compose this
 inline infix fun <A, B, C, R> F2<A, B, C>.composeForward(crossinline other: F1<C, R>): F2<A, B, R> = other compose this
 
+inline fun <A, R> F1<A, R>.partial(a: A): F0<R> = { invoke(a) }
+
+inline fun <A, B, R> F2<A, B, R>.partial(a: A, b: B): F0<R> = { invoke(a, b) }
 inline fun <A, B, R> F2<A, B, R>.partial(a: A): F1<B, R> = { invoke(a, it) }
 inline fun <A, B, R> F2<A, B, R>.partialLast(b: B): F1<A, R> = { invoke(it, b) }
 
 inline fun <A, B, C, R> F3<A, B, C, R>.partial(a: A): F2<B, C, R> = { b, c -> invoke(a, b, c) }
 inline fun <A, B, C, R> F3<A, B, C, R>.partial(a: A, b: B): F1<C, R> = { c -> invoke(a, b, c) }
+inline fun <A, B, C, R> F3<A, B, C, R>.partial(a: A, b: B, c: C): F0<R> = { invoke(a, b, c) }
 inline fun <A, B, C, R> F3<A, B, C, R>.partialLast(c: C): F2<A, B, R> = { a, b -> invoke(a, b, c) }
 inline fun <A, B, C, R> F3<A, B, C, R>.partialLast(b: B, c: C): F1<A, R> = { a -> invoke(a, b, c) }
 
 inline fun <A, B, C, D, R> F4<A, B, C, D, R>.partial(a: A): F3<B, C, D, R> = { b, c, d -> invoke(a, b, c, d) }
 inline fun <A, B, C, D, R> F4<A, B, C, D, R>.partial(a: A, b: B): F2<C, D, R> = { c, d -> invoke(a, b, c, d) }
 inline fun <A, B, C, D, R> F4<A, B, C, D, R>.partial(a: A, b: B, c: C): F1<D, R> = { d -> invoke(a, b, c, d) }
+inline fun <A, B, C, D, R> F4<A, B, C, D, R>.partial(a: A, b: B, c: C, d: D): F0<R> = { invoke(a, b, c, d) }
 inline fun <A, B, C, D, R> F4<A, B, C, D, R>.partialLast(d: D): F3<A, B, C, R> = { a, b, c -> invoke(a, b, c, d) }
 inline fun <A, B, C, D, R> F4<A, B, C, D, R>.partialLast(c: C, d: D): F2<A, B, R> = { a, b -> invoke(a, b, c, d) }
 inline fun <A, B, C, D, R> F4<A, B, C, D, R>.partialLast(b: B, c: C, d: D): F1<A, R> = { a -> invoke(a, b, c, d) }
