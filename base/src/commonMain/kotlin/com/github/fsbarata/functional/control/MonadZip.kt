85dd7d731a09f0bc0ee65b5901ebc20e62afae47
fixedlight <fixedlight@gmail.com>
2022-03-25 17:35:48 +0000

    zipT and unzipT

--- base/src/commonMain/kotlin/com/github/fsbarata/functional/control/MonadZip.kt
+++ base/src/commonMain/kotlin/com/github/fsbarata/functional/control/MonadZip.kt
@@ -2,7 +2,6 @@ package com.github.fsbarata.functional.control
 
 import com.github.fsbarata.functional.data.Functor
 import com.github.fsbarata.functional.data.tuple.Tuple2
-import kotlin.jvm.JvmName
 
 interface MonadZip<M, out A>: Monad<M, A> {
 	fun <B, R> zipWith(other: Functor<M, B>, f: (A, B) -> R): MonadZip<M, R>
@@ -11,12 +10,11 @@ interface MonadZip<M, out A>: Monad<M, A> {
 fun <M, A, B, R> zip(monad1: MonadZip<M, A>, monad2: Functor<M, B>, f: (A, B) -> R) =
 	monad1.zipWith(monad2, f)
 
-fun <M, A, B> zip(monad1: MonadZip<M, A>, monad2: Functor<M, B>) =
-	zip(monad1, monad2, ::Pair)
+fun <M, A, B> zip(monad1: MonadZip<M, A>, monad2: Functor<M, B>) = zip(monad1, monad2, ::Pair)
+fun <M, A, B> zipT(monad1: MonadZip<M, A>, monad2: Functor<M, B>) = zip(monad1, monad2, ::Tuple2)
 
 fun <M, A, B> unzip(zipped: Functor<M, Pair<A, B>>): Pair<Functor<M, A>, Functor<M, B>> =
 	Pair(zipped.map { it.first }, zipped.map { it.second })
 
-@JvmName("unzipTuple")
-fun <M, X, Y> unzip(zipped: Functor<M, Tuple2<X, Y>>): Pair<Functor<M, X>, Functor<M, Y>> =
-	Pair(zipped.map { it.x }, zipped.map { it.y })
+fun <M, X, Y> unzipT(zipped: Functor<M, Tuple2<X, Y>>): Tuple2<Functor<M, X>, Functor<M, Y>> =
+	Tuple2(zipped.map { it.x }, zipped.map { it.y })
