81d87a191c204ef2f52b944d97e4a85ce3d3156d
fixedlight <fixedlight@gmail.com>
2024-10-04 11:57:14 +0100

    fApplyN: apply a function N times

--- base/src/commonMain/kotlin/com/github/fsbarata/functional/data/monoid/Endo.kt
+++ base/src/commonMain/kotlin/com/github/fsbarata/functional/data/monoid/Endo.kt
@@ -6,3 +6,11 @@ typealias Endomorphism<A> = F1<A, A>
 
 fun <A> endoSemigroup() = Semigroup.Scope<Endomorphism<A>>(::compose)
 fun <A> endoMonoid(): Monoid<Endomorphism<A>> = monoidOf(::id, ::compose)
+
+/**
+ * Apply an endomorphism n times
+ * Equivalent to generateSequence(a, f).take(n).last()
+ */
+tailrec fun <A> fapplyN(a: A, n: Int, f: Endomorphism<A>): A =
+	if (n <= 0) a
+	else fapplyN(f(a), n - 1, f)
