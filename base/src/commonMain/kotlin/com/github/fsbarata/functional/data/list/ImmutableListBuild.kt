eb00e86212096eb46f5547fd5cc64a3ee7fbd126
fixedlight <fixedlight@gmail.com>
2024-12-08 11:21:39 +0000

    ImmutableListBuildScope - extended to MutableList

--- base/src/commonMain/kotlin/com/github/fsbarata/functional/data/list/ImmutableListBuild.kt
+++ base/src/commonMain/kotlin/com/github/fsbarata/functional/data/list/ImmutableListBuild.kt
@@ -1,39 +1,25 @@
 package com.github.fsbarata.functional.data.list
 
-class ImmutableListBuildScope<A>(sizeHint: Int = -1): MutableCollection<A>, RandomAccess {
-	private var list: MutableList<A>? = if (sizeHint >= 0) ArrayList(sizeHint) else ArrayList()
-
-	override val size: Int get() = list?.size ?: 0
-
-	override fun isEmpty(): Boolean = size == 0
+class ImmutableListBuildScope<A>(initialCapacity: Int = -1): MutableList<A>, RandomAccess {
+	private var mutableList: MutableList<A>? = if (initialCapacity >= 0) ArrayList(initialCapacity) else ArrayList()
+	private val list: List<A>? get() = mutableList
 
 	fun build(): ListF<A> {
-		val unreachableList = list ?: throw IllegalStateException("build() can only be called once")
-		list = null
+		val unreachableList = mutableList ?: throw IllegalStateException("build() can only be called once")
+		mutableList = null
 		return ListF(unreachableList)
 	}
 
-	private fun listOrError(): MutableList<A> =
-		checkNotNull(list) { "list has been built" }
+	private fun builtError(): Nothing = throw IllegalStateException("list has been built")
+	private fun listOrError(): List<A> = list ?: builtError()
 
-	operator fun get(index: Int): A = listOrError()[index]
+	override val size: Int get() = list?.size ?: 0
+	override fun isEmpty(): Boolean = size == 0
 
-	operator fun set(index: Int, element: A): A =
-		listOrError().set(index, element)
+	override operator fun get(index: Int): A = listOrError()[index]
 
-	override fun iterator(): MutableIterator<A> {
-		val iterator = listOrError().iterator()
-		return object: MutableIterator<A> by iterator {
-			override fun remove() {
-				checkNotNull(list) { "list has been built" }
-				iterator.remove()
-			}
-		}
-	}
-
-	fun indexOf(element: A): Int = list?.indexOf(element) ?: -1
-
-	fun lastIndexOf(element: A): Int = list?.lastIndexOf(element) ?: -1
+	override fun indexOf(element: A): Int = list?.indexOf(element) ?: -1
+	override fun lastIndexOf(element: A): Int = list?.lastIndexOf(element) ?: -1
 
 	override fun contains(element: A): Boolean {
 		return list?.contains(element) ?: false
@@ -43,43 +29,113 @@ class ImmutableListBuildScope<A>(sizeHint: Int = -1): MutableCollection<A>, Rand
 		return list?.containsAll(elements) ?: false
 	}
 
-	override fun add(element: A): Boolean {
-		return list?.add(element) ?: false
+	private var modCount = 0
+	private inline fun <R> modifyOrNull(block: MutableList<A>.() -> R): R? {
+		val r = mutableList?.block()
+		modCount++
+		return r
 	}
 
-	fun add(index: Int, element: A): Boolean {
-		return list?.add(index, element) != null
+	private inline fun <R> modifyOrError(block: MutableList<A>.() -> R): R {
+		val mutableList = mutableList ?: builtError()
+		val r = mutableList.block()
+		if (this.mutableList == null) builtError()
+		modCount++
+		return r
 	}
 
-	override fun remove(element: A): Boolean {
-		return list?.remove(element) ?: false
+	override fun subList(fromIndex: Int, toIndex: Int): MutableList<A> {
+		throw UnsupportedOperationException("Operation is not supported within the ImmutableListBuildScope")
 	}
 
-	fun removeAt(index: Int): A =
-		listOrError().removeAt(index)
+	override fun iterator(): MutableIterator<A> = listIterator()
+	override fun listIterator(): MutableListIterator<A> = listIterator(0)
+	override fun listIterator(index: Int): MutableListIterator<A> = ListIterator(this, 0)
 
-	override fun addAll(elements: Collection<A>): Boolean {
-		return addAll(size, elements)
+	override operator fun set(index: Int, element: A): A = modifyOrError { set(index, element) }
+
+	override fun add(element: A): Boolean = modifyOrNull { add(element) } ?: false
+	override fun add(index: Int, element: A) {
+		modifyOrError { add(index, element) }
 	}
 
-	fun addAll(index: Int, elements: Collection<A>): Boolean {
-		return when (elements.size) {
-			0 -> false
-			1 -> add(index, elements.first())
-			else -> list?.addAll(index, elements) ?: false
-		}
+	override fun addAll(elements: Collection<A>): Boolean = addAll(size, elements)
+	override fun addAll(index: Int, elements: Collection<A>): Boolean = when (elements.size) {
+		0 -> false
+		1 -> modifyOrNull { add(index, elements.first()) } != null
+		else -> modifyOrNull { addAll(index, elements) } ?: false
 	}
 
-	override fun retainAll(elements: Collection<A>): Boolean {
-		return list?.retainAll(elements) ?: false
-	}
+	override fun retainAll(elements: Collection<A>): Boolean = modifyOrNull { retainAll(elements) } ?: false
 
-	override fun removeAll(elements: Collection<A>): Boolean {
-		return list?.removeAll(elements) ?: false
-	}
+	override fun remove(element: A): Boolean = modifyOrNull { remove(element) } ?: false
+	override fun removeAt(index: Int): A = modifyOrError { removeAt(index) }
+
+	override fun removeAll(elements: Collection<A>): Boolean = modifyOrNull { removeAll(elements) } ?: false
 
 	override fun clear() {
-		listOrError().clear()
+		modifyOrError { clear() }
+	}
+
+	private class ListIterator<A>(
+		private val mutableList: ImmutableListBuildScope<A>,
+		startIndex: Int,
+	): MutableListIterator<A> {
+		var index = startIndex
+		var indexBefore = -1
+
+		private var expectedModCount = mutableList.modCount
+
+		override fun hasPrevious(): Boolean = index > 0 && mutableList.size > 0
+		override fun hasNext(): Boolean = index < mutableList.size
+
+		override fun previousIndex(): Int = index - 1
+		override fun nextIndex(): Int = index
+
+		override fun previous(): A {
+			checkModCount()
+			if (!hasPrevious()) throw NoSuchElementException()
+			indexBefore = index
+			return mutableList[--index]
+		}
+
+		override fun next(): A {
+			checkModCount()
+			if (!hasNext()) throw NoSuchElementException()
+			indexBefore = index
+			return mutableList[index++]
+		}
+
+		override fun add(element: A) {
+			checkModCount()
+			mutableList.add(index, element)
+			updateModCount()
+			indexBefore = -1
+		}
+
+		override fun remove() {
+			checkModCount()
+			check(indexBefore != -1) { "Call to previous() or next() required" }
+			mutableList.removeAt(indexBefore)
+			updateModCount()
+			indexBefore = -1
+		}
+
+		override fun set(element: A) {
+			checkModCount()
+			check(indexBefore != -1) { "Call to previous() or next() required" }
+			mutableList[indexBefore] = element
+			updateModCount()
+		}
+
+
+		fun checkModCount() {
+			if (mutableList.modCount != expectedModCount) throw ConcurrentModificationException()
+		}
+
+		fun updateModCount() {
+			expectedModCount = mutableList.modCount
+		}
 	}
 }
 
